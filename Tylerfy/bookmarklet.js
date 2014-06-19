88a67e3715262e5e1a84b0fda718b13c617dbb98
Ian Routledge <iandroutledge@hotmail.com>
2014-06-19 14:05:14 +0100

    Random querystring on bookmarklet

--- Tylerfy/bookmarklet.js
+++ Tylerfy/bookmarklet.js
@@ -1,16 +1,26 @@
 ﻿
-(function() {
+(function () {
+
+    function start() {
+        swapImages();
+        setInterval(swapImages, 2000);
+    }
+
     function swapImages() {
         var images = document.images;
         for (var i = 0; i < images.length; i++) {
             var image = images[i];
-            image.src = 'http://placekitten.com/' + image.width + '/' + image.height;
+            if (image.getAttribute('data-tylerfied') !== 'true')
+            {
+                image.setAttribute('data-tylerfied', 'true');
+                image.src = 'http://tylerfy.com/g/' + image.width + '/' + image.height + '?nocache=' + Math.round(Math.random() * 10000000);
+            }
         }
     }
 
     if(document.readyState === 'complete') {
-        swapImages();
+        start();
     } else {
-        window.addEventListener('load', swapImages);
+        window.addEventListener('load', start);
     }
 })();
